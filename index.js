a95be7ebfaa56af0a8e15b01212f7195fa25510a
Hishtalmut Computer Teacher <HcomputerT@m08.local>
2025-03-21 11:09:48 +0200

    Render Logic

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const tasks = [
-    {id:1, content:"להכין קוגל", completed:true},
-    {id:2, content:"לסדר את השולחן", completed:false},
-    {id:3, content:"לגהץ חולצות", completed:false}
+    {id:111, content:"להכין קוגל", completed:true},
+    {id:222, content:"לסדר את השולחן", completed:false},
+    {id:333, content:"לגהץ חולצות", completed:false}
 ]
+
+function renderTasks(){
+    tasksElement = tasks.map(t=>   `<div>${t.content}  ${t.completed==true?'V':'X'} </div>`)
+    tasksStr = tasksElement.join("");
+    document.body.innerHTML+=tasksStr;
+}
+
+renderTasks();
